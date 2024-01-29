91769daeda40b9153c1cf93be84aa4f2c5170e8c
LeonardSuli <leonardsuli1991@gmail.com>
2024-01-29 18:00:40 +0100

    add listodo and line-through

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default{
+  name: 'App',
+  data(){
+    return {
+      todoList:[
+        {
+          text:'Birre',
+          done: true
+        },
+        {
+          text:'Salamelle',
+          done: true
+        },
+        {
+          text:'Hamburger',
+          done: false
+        },
+        {
+          text:'Spiedini',
+          done: true
+        },
+        {
+          text:'Pesce',
+          done: false
+        },
+        {
+          text:'Melanzane',
+          done: false
+        },
+        {
+          text:'Peperoni',
+          done: true
+        },
+        {
+          text:'Anguria',
+          done: false
+        },
+        {
+          text:'Melone',
+          done: true
+        },
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <h1>Grigliata</h1>
+
+  <ul>
+    <li v-for="list in todoList">
+
+      <span :style="{textDecoration: list.done ? 'line-through' : ''}">
+        {{ list.text }}
+      </span>
+
+    </li>
+  </ul>
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
