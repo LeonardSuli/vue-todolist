cc0f4db426579726b090278d65e3132a812ecde1
LeonardSuli <leonardsuli1991@gmail.com>
2024-01-29 22:20:20 +0100

    add addlist

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ export default{
   name: 'App',
   data(){
     return {
+      newList:{
+        text: '',
+        done: false
+      },
       todoList:[
         {
           text:'Birre',
@@ -44,9 +48,16 @@ export default{
     }
   },
   methods:{
+
     removeList(index){
       console.log('rimuovi', index);
       this.todoList.splice(index, 1)
+    },
+
+    addList(){
+      console.log(this.newList.text);
+      this.todoList.unshift(this.newList);
+      this.newList = '';
     }
   }
 }
@@ -56,8 +67,13 @@ export default{
 
   <h1>Grigliata</h1>
 
+  <div>
+    <input type="text" v-model="newList.text">
+    <button @click="addList()">Aggiungi</button>
+  </div>
+
   <ul v-if="todoList.length > 0">
-    
+
     <li v-for="(list, index) in todoList">
 
       <span :style="{textDecoration: list.done ? 'line-through' : ''}">
